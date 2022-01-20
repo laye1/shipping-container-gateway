a8339deb589f8d606527dcb1c4832bb37732e6c9
laye ndiaye <layendiaye195@gmail.com>
2022-01-20 13:53:46 +0000

    first commit

---
+++ src/main/java/com/saraya/shippingapigateway/ShippingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.shippingapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saraya/shippingapigateway/ShippingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraya.shippingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
